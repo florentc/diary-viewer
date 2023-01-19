fe6cd9f790d89543874a0046cfa1d9278d30c00e
Florent C <florentc@users.noreply.github.com>
2023-01-19 21:38:20 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixoS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {self, nixpkgs, flake-utils} :
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellpkgs = pkgs.haskellPackages;
+      in {
+        formatter = pkgs.nixfmt;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            haskellpkgs.ghc
+            haskellpkgs.haskell-language-server
+            pkgs.cabal-install
+            pkgs.ormolu
+            pkgs.hpack
+          ];
+        };
+      }
+    );
+}
